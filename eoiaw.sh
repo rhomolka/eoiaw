c34e1502fa56268032e9065f5f702b97713ca7f3
Richard Homolka <77993544+rhomolka@users.noreply.github.com>
2023-10-04 00:12:33 -0500

    Cross platform gnu tools

--- eoiaw.sh
+++ eoiaw.sh
@@ -13,6 +13,24 @@ LOCKDIR=$MAINDIR/var/lockdir
 mkdir $LOCKDIR 2> /dev/null || exit
 trap "rm -rf $LOCKDIR" EXIT TERM INT
 
+UNAME_S=$(uname -s)
+case "$UNAME_S" in
+Darwin)
+    # homebrew
+    # brew install coreutils
+    GNUDATE=gdate
+    GNUSTAT=gstat
+    ;;
+Linux)
+    # normal coreutils
+    GNUDATE=date
+    GNUSTAT=stat
+    ;;
+*)
+    echo "Unsupported OS type $UNAME_S" >&2
+    exit 1
+esac
+
 for DIR in $MAINDIR/rundirs/*.d
 do
     [ ! -d $DIR ] && continue
@@ -21,7 +39,7 @@ do
     TOUCHFILE=$MAINDIR/var/touchfiles/$TIMEOFFSET
 
     [ -f $TOUCHFILE ] \
-        && [ $(gdate --date="$TIMEOFFSET ago" '+%s') -lt $(gstat -c '%Y' $TOUCHFILE) ] \
+        && [ $($GNUDATE --date="$TIMEOFFSET ago" '+%s') -lt $($GNUSTAT -c '%Y' $TOUCHFILE) ] \
         && continue
 
     for FILE in $DIR/*
